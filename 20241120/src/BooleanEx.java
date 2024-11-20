9ec6759d5c50592e2437e58a9e9cd20eb551b85c
7qrous <louis.develog@gmail.com>
2024-11-20 14:32:57 +0900

    boolean example

--- 20241120/src/BooleanEx.java
+++ 20241120/src/BooleanEx.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class BooleanEx {
+    public static void main(String[] args) {
+        
+        
+        boolean stop= true; // boolean type 변수에 boolean 값 저장
+
+        if (stop) {
+            System.out.println("중지 합니다");
+        }else{
+            System.out.println("시작 합니다");
+        }
+
+    }
 }

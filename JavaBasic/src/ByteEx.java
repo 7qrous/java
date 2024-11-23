80876ddb0a1ae5dff5b287c28bcc633bed0c791f
7qrous <louis.develog@gmail.com>
2024-11-23 13:21:19 +0900

    폴더 정리

--- JavaBasic/src/ByteEx.java
+++ JavaBasic/src/ByteEx.java
@@ -7,7 +7,7 @@ public class ByteEx {
         byte var3 = 0;
         byte var4 = 30;
         byte var5 = 127;
-        
+
         // 128은 byte타입이 저장할 수 있는 데이터의 범위를 넘어감
 //        byte var6 = 128;
 
@@ -21,11 +21,11 @@ public class ByteEx {
 
         byte value1 = 125;
         int value2 = 125;
-        
-        
+
+
         // 반복문 for 사용
         // 기본 타입 변수가 가질 수 있는 최소값 혹은 최대값을 넘어가는 연산을 진행하면 반대값이 나옴
-        for ( int i=0; i<5; i++){
+        for (int i = 0; i < 5; i++) {
             value1++; // 변수 value1의 값 1증가 ( overflow )
             value2++; // 변수 value2의 값 1증가
             System.out.println("value1 = " + value1 + ", value2 = " + value2);

a9b47c7b7d33dd0821e6feb71f1341ff8d35f56a
7qrous <louis.develog@gmail.com>
2024-11-27 15:57:39 +0900

    update

--- JavaBasic/src/_05_참조타입/퀴즈20241127/Quiz6.java
+++ JavaBasic/src/_05_참조타입/퀴즈20241127/Quiz6.java
@@ -2,7 +2,8 @@ package _05_참조타입.퀴즈20241127;
 
 import java.util.Scanner;
 
-// 사용자 입력을 통해서 배열의 크기를 지정하고, 지정된 배열의 크기만큼 1~100까지의 숫자를 입력받아 저장 후 배열의 모든 내용을 출력하는 프로그램을 작성하세요.
+// 사용자 입력을 통해서 배열의 크기를 지정하고, 지정된 배열의 크기만큼 1~100까지의 숫자를 입력받아
+// 저장 후 배열의 모든 내용을 출력하는 프로그램을 작성하세요.
 public class Quiz6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);

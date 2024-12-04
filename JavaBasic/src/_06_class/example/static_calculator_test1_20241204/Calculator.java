c212b72ab0f21bdf04ca04ff995003234dfd0895
7qrous <louis.develog@gmail.com>
2024-12-04 16:03:06 +0900

    static-test

--- JavaBasic/src/_06_class/example/static_calculator_test1_20241204/Calculator.java
+++ JavaBasic/src/_06_class/example/static_calculator_test1_20241204/Calculator.java
@@ -2,7 +2,7 @@ package _06_class.example.static_calculator_test1_20241204;
 
 public class Calculator {
     static double pi = 3.14;
-    
+
     static int plus(int x, int y) {
         return x + y;
     }
--- JavaBasic/src/_06_class/example/static_calculator_test1_20241204/Main.java
+++ JavaBasic/src/_06_class/example/static_calculator_test1_20241204/Main.java
@@ -3,7 +3,7 @@ package _06_class.example.static_calculator_test1_20241204;
 public class Main {
     public static void main(String[] args) {
         // new 키워드를 사용하지 않고 값을 static method를 통해 계산했음
-        // Calculator5의 정적 메소드를 호출하여 사용
+        // Calculator의 정적 메소드를 호출하여 사용
         // 정적 메소드는 객체 생성 없이 클래스 명을 통해 바로 사용
         double result1 = Calculator.pi * 10 * 10;
         int result2 = Calculator.plus(10, 20);
@@ -11,5 +11,12 @@ public class Main {
         System.out.println("result1: " + result1);
         System.out.println("result2: " + result2);
         System.out.println("result3: " + result3);
+
+        Calculator c1 = new Calculator();
+        Calculator c2 = new Calculator();
+        c1.pi=2;
+        System.out.println(c1.pi);
+        System.out.println(c2.pi);
+
     }
 }
